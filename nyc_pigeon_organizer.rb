e66c01f918aeacb9d7308d5c14cac83f3ef6b573
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-28 22:31:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_hash = {}
+  data.each do |about, hash|
+    hash.each do |details, array|
+      array.each do |name|
+        if !organized_hash.has_key?(name)
+          organized_hash[name] = {}
+        end
+        if !organized_hash[name].has_key?(about)
+          organized_hash[name][about] = []
+        end
+        if !organized_hash[name][about].include?(details)
+          organized_hash[name][about].push(details.to_s)
+        end
+      end
+    end
+  end
+  organized_hash
 end
